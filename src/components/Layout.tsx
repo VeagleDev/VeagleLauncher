cac93474561ab8f16fc05980a91c0b35582fa4a6
Nicolasse <thecrinten@gmail.com>
2023-03-05 14:24:03 +0100

    components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,9 @@
+function Layout() {
+    return (
+        <div className="Layout">
+          <h1>Layout</h1>
+        </div>
+    );
+  }
+  
+  export default Layout;
--- src/renderer.ts
+++ src/renderer.ts
@@ -2,7 +2,6 @@
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from "./app";
-import "./login";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
