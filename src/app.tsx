f71be924ea457f0020f81fffb67fd1b52fe4217c
pierrbt <pierre08bt@gmx.co.uk>
2023-03-05 14:20:37 +0100

    TOUT login

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,11 @@
 import logo from "./static/logo.svg";
+import Layout from "./components/Layout";
+
 
 function App() {
   return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/app.tsx</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Layout />
       </div>
   );
 }
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+//import Menu from "./Menu"
+
+function Layout() {
+    return (
+        <>
+            Hello World !
+        </>
+    );
+}
+
+export default Layout;
