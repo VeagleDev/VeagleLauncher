59a2ae8a63f6899a9d2746120019759c151e176f
pierrbt <contact@veagle.fr>
2023-03-08 11:01:47 +0100

    Fetch des infos

--- src/renderer.ts
+++ src/renderer.ts
@@ -4,13 +4,55 @@ import App from "./app";
 
 import "./login";
 
+// @ts-ignore
 const preferences = api.getCredentials();
 if(preferences)
 {
     console.log(preferences);
     const { id, server, token } = preferences.credentials;
+    const installedGames = preferences.games;
 
-    //fetch()
+    fetch(server + "/games/list", {
+        method: "POST",
+        body: JSON.stringify({
+            id: id,
+            token: token
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+
+
+            const games = data;
+
+            for(const game of games)
+            {
+                game.installed = false;
+                game.path = "";
+            }
+
+            for(const installedGame of installedGames)
+            {
+                for(const game of games)
+                {
+                    if(installedGame.id === game.id)
+                    {
+                        game.installed = true;
+                        game.path = installedGame.path;
+                        break;
+                    }
+                }
+            }
+
+            console.log(games);
+
+        })
+        .catch(err => {
+            console.error(err);
+        });
 }
 else
 {
