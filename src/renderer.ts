f271a712c2a26a57df6c0dc956a26ea2120014db
pierrbt <contact@veagle.fr>
2023-03-08 11:09:59 +0100

    Fichier de start.ts

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,63 +1,7 @@
-import ReactDOM from 'react-dom/client';
 import './css/app.css';
-import App from "./app";
 
 import "./login";
-
-// @ts-ignore
-const preferences = api.getCredentials();
-if(preferences)
-{
-    console.log(preferences);
-    const { id, server, token } = preferences.credentials;
-    const installedGames = preferences.games;
-
-    fetch(server + "/games/list", {
-        method: "POST",
-        body: JSON.stringify({
-            id: id,
-            token: token
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(res => res.json())
-        .then(data => {
-
-
-            const games = data;
-
-            for(const game of games)
-            {
-                game.installed = false;
-                game.path = "";
-            }
-
-            for(const installedGame of installedGames)
-            {
-                for(const game of games)
-                {
-                    if(installedGame.id === game.id)
-                    {
-                        game.installed = true;
-                        game.path = installedGame.path;
-                        break;
-                    }
-                }
-            }
-
-            console.log(games);
-
-        })
-        .catch(err => {
-            console.error(err);
-        });
-}
-else
-{
-    console.error("No preferences found");
-}
+import "./start";
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
 
